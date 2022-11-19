c0a2cc5e4eb0b1ff9738553d660d061098029138
Erick Pedrosa <84411590+ErickPedrosa@users.noreply.github.com>
2022-11-19 11:42:39 -0300

    Add files via upload

---
+++ TrimestralOld/Cliente.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trimestral
+{
+    public class Cliente
+    {
+        private string nome;
+        private string email;
+        private Mesa mesa;
+
+        public Mesa Mesa { get { return mesa; } set { mesa = value; } }
+        public string Nome { get { return nome; } set { nome = value; } }
+        public string Email { get { return email; } set { email = value; } }
+
+        public Cliente(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+
+        public void ImprimeCliente()
+        {
+            Console.WriteLine($"Nome: {Nome}");
+            Console.WriteLine($"Email: {Email}");
+            /*if (Mesa != null)
+            {
+                Console.WriteLine($"Número da Mesa: {Mesa.NumMesa}");
+            }*/
+            
+        }
+
+
+    }
+}
