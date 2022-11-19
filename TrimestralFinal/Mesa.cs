e5eac70ff65b7299620d4f6bd154be627ecfb744
Erick Pedrosa <84411590+ErickPedrosa@users.noreply.github.com>
2022-11-19 11:41:45 -0300

    Add files via upload

---
+++ TrimestralFinal/Mesa.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trimestral
+{
+    public class Mesa
+    {
+        private static int incremento = 1;
+
+        private int tamanhoDaMesa = 4;
+        private Cliente[] clientes = null;
+        private int numMesa;
+        private string data;
+        private bool reservado;
+        private ComandaBebidas comandaBebidas;
+        private ComandaComidas comandaComidas;
+
+        public ComandaBebidas ComandaBebidas { get { return comandaBebidas; } private set { comandaBebidas = value; } }
+        public ComandaComidas ComandaComidas { get { return comandaComidas; } private set { comandaComidas = value; } }
+
+        public int TamanhoDaMesa { get { return tamanhoDaMesa; } private set { tamanhoDaMesa = value; } }
+        public int NumMesa { get { return numMesa; } private set { numMesa = value; } }
+        public string Data { get { return data; } set { data = value; } }
+        public bool Reservado { get { return reservado; } private set { reservado = value; } }
+        public Cliente[] Clientes { get { return clientes; } private set { clientes = value; } }
+
+        public Mesa()
+        {
+            numMesa = incremento;
+            incremento++;
+            Reservado = false;
+            Data = DateTime.Now.ToString();
+            ComandaBebidas = new ComandaBebidas();
+            ComandaComidas = new ComandaComidas();
+        }
+
+        public bool Reservar()
+        {
+            if (Reservado)
+            {
+                return false;
+            }
+            else
+            {
+                Reservado = true;
+                tamanhoDaMesa = 2;
+                Data = DateTime.Now.ToString();
+                return true;
+            }
+        }
+        public bool Reservar(int numDeClientes)
+        {
+            if (Reservado)
+            {
+                return false;
+            }
+            else
+            {
+                Reservado = true;
+                TamanhoDaMesa = numDeClientes;
+                Data = DateTime.Now.ToString();
+                return true;
+            }
+        }
+
+        /*public void AdicionarClientes(Cliente[] clientes)
+        {
+            this.Clientes = clientes;
+
+        }*/
+        public bool AdicionarClientes(string nome, string email)
+        {
+
+            Cliente c = new Cliente(nome, email);
+            
+
+            if (Reservado == true)
+            {
+                c.Mesa = this;
+
+                if (Clientes == null)
+                {
+                    Clientes = new Cliente[1];
+                    Clientes[0] = c;
+
+                    return true;
+                }
+                else if ( !(Clientes == null) && (Clientes.Length < TamanhoDaMesa) )
+                {
+                    Cliente[] clientesBuff = new Cliente[Clientes.Length + 1];
+
+                    for (int i = 0; i < Clientes.Length; i++)
+                    {
+                        clientesBuff[i] = Clientes[i];
+                    }
+                    clientesBuff[Clientes.Length] = c;
+
+                    Clientes = clientesBuff;
+
+                    return true;
+
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            else
+            {
+                return false;
+            }
+
+            
+
+
+        }
+        public bool AdicionarClientes(Cliente c)
+        {
+            
+            if (Reservado == true)
+            {
+                c.Mesa = this;
+                if (Clientes == null)
+                {
+                    Clientes = new Cliente[1];
+                    Clientes[0] = c;
+
+                    return true;
+                }
+                else if (!(Clientes == null) && (Clientes.Length < TamanhoDaMesa))
+                {
+                    Cliente[] clientesBuff = new Cliente[Clientes.Length + 1];
+
+                    for (int i = 0; i < Clientes.Length; i++)
+                    {
+                        clientesBuff[i] = Clientes[i];
+                    }
+                    clientesBuff[Clientes.Length] = c;
+
+                    Clientes = clientesBuff;
+
+                    return true;
+
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+
+        public bool RemoverClientes(Cliente c)
+        {
+            if (Clientes == null)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 0; i < Clientes.Length; i++)
+                {
+                    if (Clientes[i] == c)
+                    {
+                        Cliente[] buffer = new Cliente[Clientes.Length - 1];
+
+                        for (int j = 0; j < i; j++)
+                        {
+                            buffer[j] = Clientes[j];
+                        }
+                        for (int j = i; j < buffer.Length; j++)
+                        {
+                            buffer[j] = Clientes[j + 1];
+                        }
+                        Clientes = buffer;
+
+                        return true;
+                    }
+
+                }
+
+                return false;
+            }
+        }
+        public bool RemoverClientes(string email)
+        {
+            if (Clientes == null)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 0; i < Clientes.Length; i++)
+                {
+                    if (Clientes[i].Email == email)
+                    {
+                        RemoverClientes(Clientes[i]);
+                    }
+                }
+            }
+            return false;
+              
+        }
+        
+        
+        public void AnotarPedido(string pedido, double preco, int tipo)
+        {
+            if (tipo == 0)
+            {
+                ComandaBebidas.AnotarPedido(pedido, preco);
+            }
+            else if(tipo == 1)
+            {
+                ComandaComidas.AnotarPedido(pedido, preco);
+            }
+            
+        }
+
+
+        public void ImprimeMesa()
+        {
+            Console.WriteLine($"Número da Mesa: {NumMesa}");
+            Console.WriteLine($"Data: {Data}");
+            Console.WriteLine($"Reservado: {Reservado}");
+            if(Clientes != null)
+            {
+                Console.WriteLine($"Número de clientes: {Clientes.Length}");
+
+                for (int i = 0; i < Clientes.Length; i++)
+                {
+                    Console.WriteLine($"\nCliente {i + 1}: ");
+                    Clientes[i].ImprimeCliente();
+                }
+
+                Console.WriteLine($"\nConsumo da mesa:");
+                Console.WriteLine($"\nComidas:");
+                ComandaComidas.ListarConsumo();
+                Console.WriteLine($"\nBebidas:");
+                ComandaBebidas.ListarConsumo();
+            }
+
+        }
+    }
+}
---
+++ TrimestralFinal/Program.cs
@@ -0,0 +1,382 @@
+﻿using System.Linq.Expressions;
+using System.Text;
+using System;
+
+/*
+ * Integrantes do grupo:
+ * Erick Pedrosa Barreto
+ * Gabrielle Maia de Andrade Souza
+*/
+
+namespace Trimestral
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /* ----- Debug Comanda ----- */
+            /*
+            Comanda comanda = new Comanda();
+
+            comanda.AnotarPedido("Churrasco", 20);
+            comanda.AnotarPedido("Vinho", 60);
+            comanda.AnotarPedido("Champanhe", 100);
+            Console.WriteLine(comanda.Calcular10Porcento());
+            Console.WriteLine(comanda.DividirConta(2));
+
+            comanda.ListarConsumo();
+            */
+
+
+            /* ----- Debug Cliente ----- */
+            /*
+            Cliente cliente = new Cliente("Erick", "erick@gmail.com");
+            Mesa mesa = new Mesa();
+            
+            mesa.Reservar(3);
+            mesa.AdicionarClientes(cliente);
+
+            cliente.ImprimeCliente();
+            mesa.Clientes[0].ImprimeCliente();
+            */
+
+
+            /* ----- Debug Mesa ----- */
+            /*
+            Mesa mesa = new Mesa();
+
+            Console.WriteLine(mesa.Reservado);
+            Console.WriteLine(mesa.AdicionarClientes("J", "J@g.com"));
+            mesa.Reservar(2);
+            Console.WriteLine(mesa.Reservado);
+            Console.WriteLine(mesa.AdicionarClientes("L", "L@g.com"));
+            Console.WriteLine(mesa.AdicionarClientes("M", "M@g.com"));
+            Console.WriteLine(mesa.AdicionarClientes("N", "N@g.com"));
+            mesa.AnotarPedido("a", 10);
+            mesa.AnotarPedido("b", 0.50);
+            mesa.AnotarPedido("c", 1540);
+            mesa.Comanda.ListarConsumo();
+            */
+
+            /* ----- Debug Restaurante ----- */
+            /*                                           
+            Restaurante r = new Restaurante("Bill's", "R. das Cerejeiras, 144", 5); // Zero ou menos mesas levanta uma Exception;
+
+            Console.WriteLine(r.Nome);
+            Console.WriteLine(r.Endereco);
+            Console.WriteLine(r.AdicionaOuRemoveMesas(0));
+            Console.WriteLine(r.AdicionaOuRemoveMesas(-5));
+            Console.WriteLine(r.AdicionaOuRemoveMesas(-10));
+            Console.WriteLine(r.AdicionaOuRemoveMesas(-3));
+            Console.WriteLine(r.Mesa.Length);
+            Console.WriteLine(r.AdicionaOuRemoveMesas(2));
+            Console.WriteLine(r.Mesa.Length);
+
+            r.Mesa[0].Reservar(4);
+            r.Mesa[1].Reservar(1);
+            r.Mesa[3].Reservar(2);
+
+            r.Mesa[0].AdicionarClientes("Vivi", "v@gmail.com");
+            r.Mesa[0].AdicionarClientes("Honda", "h@gmail.com");
+            r.Mesa[0].AdicionarClientes("El Maia", "l@gmail.com");
+            r.Mesa[0].AdicionarClientes("João", "j@gmail.com");
+            r.Mesa[0].RemoverClientes("j@gmail.com");
+
+            r.Mesa[1].AdicionarClientes("Erick", "e@gmail.com");
+            r.Mesa[1].AdicionarClientes("Gabi", "g@gmail.com"); // Retorna False pois a reserva é para 1;
+
+            r.Mesa[3].AdicionarClientes("Juju", "j@gmail.com");
+            r.Mesa[3].AdicionarClientes("Kaio", "k@gmail.com");
+
+            r.Mesa[0].AnotarPedido("Espaguete integral ao alho e óleo", 35);
+            r.Mesa[0].AnotarPedido("Bolo de carne vegano", 40);
+            r.Mesa[0].AnotarPedido("Estrogonofe de berinjela", 15);
+            r.Mesa[1].AnotarPedido("Filé de frango com molho de mostarda", 30);
+            r.Mesa[1].AnotarPedido("Rocambole de carne moída", 45);
+            r.Mesa[3].AnotarPedido("Frango xadrez", 30);
+            r.Mesa[3].AnotarPedido("Suflê de Chuchu", 25);
+
+            Console.WriteLine("\n\n");
+            r.ImprimeRestaurante();
+            */
+
+
+
+            Menu();
+
+
+        }
+
+        public static void Menu()
+        {
+            bool ativo = true;
+
+            Restaurante rest = new Restaurante("Buchinho Cheio", "Av. Presidente Antonio Carlos", 4);
+
+            do
+            {
+                EscreveLinha(50);
+                Console.WriteLine($"Bem vindos ao Restaurante {rest.Nome}");
+                EscreveLinha(50);
+
+                Opcoes();
+                EscreveLinha(50);
+
+                Console.WriteLine("Qual operação você deseja realizar: ");
+                int op = int.Parse(Console.ReadLine());
+
+                switch (op)
+                {
+                    case 0:
+                        ativo = false;
+                        break;
+                    case 1:
+                        EscreveLinha(50);
+                        EscreveLinha(50);
+                        rest.ImprimeRestaurante();
+                        EscreveLinha(50);
+                        EscreveLinha(50);
+                        Console.WriteLine("");
+
+                        break;
+
+
+                    case 2:
+                        EscreveLinha(50);
+                        EscreveLinha(50);
+                        Console.WriteLine($"Digite o número de mesas que irão aumentar (digite um número negativo para diminuir): ");
+                        int n = int.Parse(Console.ReadLine());
+                        rest.AdicionaOuRemoveMesas(n);
+                        EscreveLinha(50);
+                        EscreveLinha(50);
+                        Console.WriteLine("");
+                        break;
+
+
+                    case 3:
+                        EscreveLinha(50);
+                        EscreveLinha(50);
+                        Console.WriteLine($"Digite o número da mesa que você deseja reservar: ");
+                        int x = int.Parse(Console.ReadLine());
+                        Console.WriteLine($"Digite o número de pessoas para a mesa que você deseja reservar: ");
+                        int numPessoas = int.Parse(Console.ReadLine());
+                        rest.Mesa[x - 1].Reservar(numPessoas);
+                        EscreveLinha(50);
+                        EscreveLinha(50);
+                        Console.WriteLine("");
+                        break;
+
+
+                    case 4:
+                        EscreveLinha(50);
+                        EscreveLinha(50);
+                        Console.WriteLine($"Digite o número da mesa que você o cliente deseja se assentar: ");
+                        int y = int.Parse(Console.ReadLine());
+                        y = y - 1;
+                        if(rest.Mesa[y].Reservado)
+                        {
+                            Console.WriteLine("Mesa reservada corretamente\n");
+                            Console.WriteLine($"Digite o nome do cliente: ");
+                            string nome = Console.ReadLine();
+                            Console.WriteLine($"Digite o email do cliente: ");
+                            string email = Console.ReadLine();
+                            rest.Mesa[y].AdicionarClientes(nome, email);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Mesa não reservada. Impossível assentar-se!!!");
+                            
+                        }
+                        EscreveLinha(50);
+                        EscreveLinha(50);
+                        Console.WriteLine("");
+                        break;
+
+
+                    case 5:
+                        EscreveLinha(50);
+                        EscreveLinha(50);
+                        Console.WriteLine($"Digite o número da mesa que você deseja remover um cliente: ");
+                        int z = int.Parse(Console.ReadLine());
+                        z = z - 1;
+                        if (rest.Mesa[z].Reservado)
+                        {
+                            Console.WriteLine("Mesa reservada corretamente\n");
+                            Console.WriteLine($"Digite o email do cliente: ");
+                            string email = Console.ReadLine();
+                            rest.Mesa[z].RemoverClientes(email);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Mesa não reservada, portanto não há ninguem para remover!!!");
+
+                        }
+                        EscreveLinha(50);
+                        EscreveLinha(50);
+                        Console.WriteLine("");
+                        break;
+
+
+                    case 6:
+                        EscreveLinha(50);
+                        EscreveLinha(50);
+                        Console.WriteLine($"Digite o número da mesa que deseja fazer um pedido: ");
+                        int a = int.Parse(Console.ReadLine());
+                        a = a - 1;
+
+                        if (rest.Mesa[a].Reservado)
+                        {
+                            Console.WriteLine("Mesa reservada corretamente\n");
+
+                            Console.WriteLine($"Digite 0 para fazer o pedido de uma bebida e 1 para fazer o pedido de uma comida: ");
+                            int tipo = int.Parse(Console.ReadLine());
+
+                            Console.WriteLine($"Digite o pedido da mesa: ");
+                            string pedido = Console.ReadLine();
+                            Console.WriteLine($"Digite o valor do pedido: ");
+                            double valor = double.Parse(Console.ReadLine());
+
+                            rest.Mesa[a].AnotarPedido(pedido, valor, tipo);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Mesa não reservada, portanto não há ninguem para fazer um pedido!!!");
+
+                        }
+
+                        EscreveLinha(50);
+                        EscreveLinha(50);
+                        Console.WriteLine("");
+                        break;
+
+
+                    case 7:
+                        EscreveLinha(50);
+                        EscreveLinha(50);
+                        Console.WriteLine($"Digite o número da mesa que você deseja listar o consumo: ");
+                        int b = int.Parse(Console.ReadLine());
+                        b = b - 1;
+
+                        if (rest.Mesa[b].Reservado)
+                        {
+                            Console.WriteLine("Mesa reservada corretamente\n");
+                            
+
+                            Console.WriteLine($"\nConsumo da mesa:");
+                            Console.WriteLine($"\nComidas:");
+                            rest.Mesa[b].ComandaComidas.ListarConsumo();
+                            Console.WriteLine($"\nBebidas:");
+                            rest.Mesa[b].ComandaBebidas.ListarConsumo();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Mesa não reservada, portanto não há como listar o consumo!!!");
+
+                        }
+
+                        EscreveLinha(50);
+                        EscreveLinha(50);
+                        Console.WriteLine("");
+                        break;
+
+
+
+                    case 8:
+                        EscreveLinha(50);
+                        EscreveLinha(50);
+                        Console.WriteLine($"Digite o número da mesa que você deseja calcular os 10%: ");
+                        int c = int.Parse(Console.ReadLine());
+                        c = c - 1;
+
+                        if (rest.Mesa[c].Reservado)
+                        {
+
+                            double dezPorcento = rest.Mesa[c].ComandaBebidas.Calcular10Porcento() + rest.Mesa[c].ComandaComidas.Calcular10Porcento();
+
+                            Console.WriteLine($"\nOs 10% da mesa {c} é R${dezPorcento.ToString("0.00")} ");
+                        
+                        }
+                        else
+                        {
+                            Console.WriteLine("Mesa não reservada, portanto não é possivel calcular os 10%!!!");
+
+                        }
+
+                        EscreveLinha(50);
+                        EscreveLinha(50);
+                        Console.WriteLine("");
+                        break;
+
+
+                    case 9:
+                        EscreveLinha(50);
+                        EscreveLinha(50);
+                        Console.WriteLine($"Digite o número da mesa que deseja dividir a conta: ");
+                        int d = int.Parse(Console.ReadLine());
+                        d = d - 1;
+
+                        if (rest.Mesa[d].Reservado)
+                        {
+                            Console.WriteLine($"Digite o número de pessoas que irão dividir a conta: ");
+                            int numClientes = int.Parse(Console.ReadLine());
+
+                            double valorBeb = rest.Mesa[d].ComandaBebidas.DividirConta(numClientes);
+                            double valorCom = rest.Mesa[d].ComandaComidas.DividirConta(numClientes);
+                            double valorTot = valorBeb + valorCom;
+
+
+                            Console.WriteLine($"O valor a pagar por cada cliente por bebidas: R${valorBeb.ToString("0.00")}");
+                            Console.WriteLine($"O valor a pagar por cada cliente por comidas: R${valorCom.ToString("0.00")}");
+                            Console.WriteLine($"\nO valor a pagar por cada cliente no total: R${valorTot.ToString("0.00")}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Mesa não reservada, portanto não há ninguem para dividir a conta!!!");
+
+                        }
+
+                        EscreveLinha(50);
+                        EscreveLinha(50);
+                        Console.WriteLine("");
+                        break;
+
+
+
+                    default:
+                        break;
+                }
+
+
+                 
+
+
+            } while (ativo);
+        }
+
+        public static void Opcoes()
+        {
+            Console.WriteLine(@"1 - Imprimir situação atual do restaurante
+2 - Aumentar/Reduzir o número de mesas
+3 - Reservar uma mesa
+4 - Adicionar um cliente a uma mesa
+5 - Remover um cliente de uma mesa
+6 - Fazer um pedido
+7 - Listar a conta total de uma mesa
+8 - Calcular 10%
+9 - Dividir a conta
+0 - Sair do Restaurante");
+        }
+
+        public static void EscreveLinha(int x)
+        {
+            for (int i = 0; i < x; i++)
+            {
+                Console.Write('-');
+            }
+
+            Console.WriteLine("");
+
+        }
+
+    }
+}
